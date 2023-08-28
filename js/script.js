16609dfa60ebdef8229244da93664ec882beb198
Mooniemd <doragondoshiro@gmail.com>
2023-08-28 15:09:27 -0400

    feat: added main code and notes for them

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
+// até a linha 26 são os códigos base de checagem e definição
+
 const blocks = Array.from(document.querySelectorAll('.block'));
 const jogadorDisplay = document.querySelector('.display-jogador');
 const reiniciarBtn = document.querySelector('#reiniciar');
-
 
 var tabuleiro = [
     ['', '', ''],
@@ -24,3 +25,58 @@ const condicaoDeVitoria = [
     [0, 4, 8],
     [2, 4, 6]
  ];
+
+// define quem tá fazendo o movimento
+ const movimentoValido = (block) => {
+    return block.innerText !== 'X' && block.innerText !== 'O';
+};
+
+// dá a possibilidade do jogador atual mexer no tabuleiro
+const atualizarTabuleiro =  (index) => {
+    tabuleiro[index] = jogadorAtual;
+ }
+
+//código que muda o jogador conforme o click (não tá funcionando ainda)
+ const mudarJogador = () => {
+    jogadorDisplay.classList.remove(`Jogador${jogadorAtual}`);
+    currentPlayer = jogadorAtual === 'X' ? 'O' : 'X';
+    jogadorDisplay.innerText = jogadorAtual;
+    jogadorDisplay.classList.add(`Jogador${jogadorAtual}`);
+}
+
+//código pra mostrar de quem é a ação e no final muda o jogador (não tá funcionando também)
+const acao = (block , index) => {
+    if (movimentoValido(block) && jogoAtivo) {
+      block.innerText = jogadorAtual;
+      block.classList.add(`Jogador${jogadorAtual}`);
+      updateBoard(index);
+      handleResultValidation();
+      mudarJogador();
+    }
+  };
+
+  blocks.forEach( (block, index) => {
+    block.addEventListener('click', () => acao(block, index));
+});
+
+// reinicia o tabuleiro checando quem tá jogando e setando o texto pra vazio.
+const reiniciarTabuleiro = () => {
+    tabuleiro = [
+        ['', '', ''],
+        ['', '', ''],
+        ['', '', ''],
+     ];
+    jogoAtivo = true;
+
+    if (jogadorAtual === 'O') {
+        mudarJogador();
+    }
+
+    blocks.forEach(block => {
+        block.innerText = '';
+        block.classList.remove('playerX');
+        block.classList.remove('playerO');
+    });
+}
+
+reiniciar.addEventListener('click', reiniciarTabuleiro);
